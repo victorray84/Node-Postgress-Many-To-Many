530a75cb2341772b81cd302ca918409ad9b70fcf
Victor Ray <victor.ray22@gmail.com>
2020-05-22 15:26:27 +0530

    Update index.js file

--- index.js
+++ index.js
@@ -11,24 +11,32 @@ app.use(bodyParser.json());
 app.use("/tags", tagRoutes);
 app.use("/messages", messagesRoutes);
 
-// catch 404 and forward to error handler
-if (app.get("env") === "development") {
-  app.use((req, res, next) => {
-    var err = new Error("Not Found");
-    err.status = 404;
-    return next(err);
-  });
-}
+// // catch 404 and forward to error handler
+// if (app.get("env") === "development") {
+//   app.use((req, res, next) => {
+//     var err = new Error("Not Found");
+//     err.status = 404;
+//     return next(err);
+//   });
+// }
 
-// development error handler
-// will print stacktrace
-if (app.get("env") === "development") {
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    return res.json({
-      message: err.message,
-      error: err,
-    });
+// // development error handler
+// // will print stacktrace
+// if (app.get("env") === "development") {
+//   app.use((err, req, res, next) => {
+//     res.status(err.status || 500);
+//     return res.json({
+//       message: err.message,
+//       error: err,
+//     });
+//   });
+// }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
