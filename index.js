401267996f3f3743a3e991caf988e5e3468f040a
Victor Ray <victor.ray22@gmail.com>
2020-05-22 15:50:30 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -11,26 +11,26 @@ app.use(bodyParser.json());
 app.use("/tags", tagRoutes);
 app.use("/messages", messagesRoutes);
 
-// // catch 404 and forward to error handler
-// if (app.get("env") === "development") {
-//   app.use((req, res, next) => {
-//     var err = new Error("Not Found");
-//     err.status = 404;
-//     return next(err);
-//   });
-// }
+// catch 404 and forward to error handler
+if (app.get("env") === "development") {
+  app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    return next(err);
+  });
+}
 
-// // development error handler
-// // will print stacktrace
-// if (app.get("env") === "development") {
-//   app.use((err, req, res, next) => {
-//     res.status(err.status || 500);
-//     return res.json({
-//       message: err.message,
-//       error: err,
-//     });
-//   });
-// }
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({
+      message: err.message,
+      error: err,
+    });
+  });
+}
 
 if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
